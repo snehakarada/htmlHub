6f80cf17a607c44cfa929e2c757e9297671a4f00
bhagya karada <146413665+snehakarada@users.noreply.github.com>
2025-05-20 18:00:32 +0530

    moving everything

--- DOM/script/snake.js
+++ DOM/script/snake.js
@@ -2,8 +2,10 @@ const drawGrid = () => {
   for (let i = 0; i < 2500; i++) {
     const child = document.createElement('div');
     child.style.backgroundColor = 'yellow';
+    child.style.border = '1px solid black';
     const tag = document.getElementById("parent");
     child.id = `box${i}`;
+
     tag.append(child);
   }
 };
@@ -32,14 +34,26 @@ const updateGrid = (position) => {
   }
 };
 
-const runGame = (position, step, score, place) => {
+const canMove = (newHead, snake, intervalId) => {
+  console.log(newHead, snake.at(-2));
+  const isOut = (newHead % 50) === 0 && snake.at(-2) === (newHead - 2);
+  const vertical = newHead >= 2450 && newHead <= 2500 && snake.at(-2) === (newHead - 100);
+  if (isOut || vertical || newHead < 0 || (newHead - 1) % 50 === 0) {
+    console.log('out');
+    clearInterval(intervalId);
+  }
+};
+
+const runGame = (position, step, score, place, intervalId) => {
   const head = position.at(-1);
   const newHead = head + step;
+  canMove(newHead, position, intervalId);
   let isGotPoint = false;
+  let newTarget = place;
 
   if (newHead === place) {
     position.push(newHead);
-    place = generateNewTarget(position);
+    newTarget = generateNewTarget(position);
     isGotPoint = true;
 
   } else {
@@ -48,9 +62,9 @@ const runGame = (position, step, score, place) => {
   }
 
   updateGrid(position);
-  placeTarget(place);
+  placeTarget(newTarget);
 
-  return isGotPoint ? score + 1 : score;
+  return [isGotPoint ? score + 1 : score, newTarget];
 };
 
 const placeTarget = (place) => {
@@ -62,18 +76,21 @@ const main = () => {
   let place;
   let step = 1;
   let score = 0;
-
+  console.log('inside main on load');
   drawGrid();
 
   const position = [0, 1];
   place = generateNewTarget(position);
 
-  document.addEventListener('keydown', (event) => {
+  document.body.addEventListener('keydown', (event) => {
     step = move(event);
   });
 
-  setInterval(() => {
-    score = runGame(position, step, score, place);
+
+
+  const intervalId = setInterval(() => {
+    [score, place] = runGame(position, step, score, place, intervalId);
+    // clearInterval(intervalId);
     const scoreCard = document.getElementById("score");
     scoreCard.textContent = `score : ${score}`;
   }, 100);
---
+++ diary/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <input type="text" placeholder="Enter your name" />
+      <input type="number" placeholder="Enter Password" />
+      <button type="submit">submit</button>
+    </>
+  );
+}
+
+export default App;
